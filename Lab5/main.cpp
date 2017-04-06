5e48c64d2072d4ab337882d94a3ffaf2168e707e
Ivan <vnstrlkv@gmail.com>
2017-04-06 18:24:05 +0700

    -_-

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -9,15 +9,43 @@ struct list
 
 
 
-list* my_malloc(char *a)
+void* my_malloc(int size ,char *memory)
 {
-	printf("\n%d", ((list*)a)->size);
-	return 0;
+	int m = 200;
+	list temp;
+	if (first_list.next == NULL)
+	{
+	
+		temp.size = sizeof(list);
+		temp.next = NULL;
+		*((list*)(memory+ first_list.size)) = temp;
+		first_list.next = (list*)(memory + first_list.size);
+		first_list.size -= temp.size;
+	}
+	else
+	{
+		temp.size = sizeof(list);
+		temp.next = (first_list.next+sizeof(list));
+		*((list*)(memory + first_list.size)) = temp;
+		first_list.next = (list*)(memory + first_list.size);
+		first_list.size -= temp.size;
+	}
+
+	return first_list.next;
 }
 
 void my_free()
 {
 
+}
+
+void print_next(list* a)
+{
+	while (a->next != NULL)
+	{
+		print_next(a->next);
+		printf("%d", a->size);
+	}
 }
 
 void main()
@@ -26,7 +54,7 @@ void main()
 	char *memory = (char*)malloc(m);
 	for (int i = 0; i < m; i++)
 	{
-		memory[i] = '0';
+		memory[i] ='0';
 	}
 	first_list.size = m-sizeof(list);
 	*((list*)memory) = first_list;
@@ -34,5 +62,25 @@ void main()
 	{
 		printf("%c ", memory[i]);
 	}
-	my_malloc(memory);
+		printf("\n");
+	printf("%d", first_list.size);
+	printf("\n");
+
+	for (int i = 0; i < 5; i++)
+	{
+		my_malloc(6, memory);
+		printf("\n ");
+		for (int i = 0; i < m; i++)
+		{
+			printf("%c ", memory[i]);
+		}
+		printf("\n");
+		printf("%d", first_list.size);
+		printf("\n");
+	}
+	printf("\n");
+	print_next(first_list.next);
+
+
+
 }
